7a5f9c0ca07d071fd23997bf2c1dd183424d7fbc
Mateusz Pietrzak <matteeo03@gmail.com>
2026-01-06 03:38:09 +0100

    Deploy Flutter web app to GitHub Pages. Final.

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a5cb96369ef86c7e85abf5d662a1ca5d897750
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2222749915"
+    serviceWorkerVersion: "2607370141"
   }
 });
